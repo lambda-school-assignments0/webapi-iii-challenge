f9eabcf65db1184724f1452ecc56cf0e0f187930
schang1146 <sammychang2185@gmail.com>
2019-07-30 22:17:27 -0700

    added framework for custom middleware

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = 'express';
+const express = "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {
+function logger(req, res, next) {}
 
-};
+function validateUserId(req, res, next) {}
+
+function validateUser(req, res, next) {}
+
+function validatePost(req, res, next) {}
 
 module.exports = server;
