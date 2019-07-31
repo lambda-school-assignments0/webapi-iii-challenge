14ad403f13b5b05d37bf8b9f28bbb7443b329abf
schang1146 <sammychang2185@gmail.com>
2019-07-30 22:54:17 -0700

    finished logger()

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-const express = "express";
+const express = require("express");
 
 const server = express();
+
+// global 'use' pipeline
+server.use(logger);
 
 server.get("/", (req, res) => {
     res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
-function logger(req, res, next) {}
+// custom middleware
+function logger(req, res, next) {
+    const now = new Date().toGMTString();
+    console.log(req.method, req.url, now);
+    next();
+}
 
 function validateUserId(req, res, next) {}
 
